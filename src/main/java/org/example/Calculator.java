c1cbb6936068d8a1174eb7a98b9bb95ac9340d0c
katechek <62990618+katechek@users.noreply.github.com>
2024-05-27 11:43:41 +0300

    Update Calculator.java

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Scanner; 
 
 public class Calculator {
     public static void main( String[] args ) throws IOException {
